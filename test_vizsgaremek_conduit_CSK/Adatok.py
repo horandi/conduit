abe248cef8062a7ddd6710c173ba0e1881810332
Kitti <csmzkt11.11@gmail.com>
2023-04-08 16:32:49 +0200

    Vizsgaremek

--- test_vizsgaremek_conduit_CSK/Adatok.py
+++ test_vizsgaremek_conduit_CSK/Adatok.py
@@ -1,5 +1,25 @@
 user = {
     "name": "Kitti",
-    "email": "tesztkitti6@teszt.com",
+    "email": "tesztkitti@teszt.com",
     "password": "Teszt123"
+}
+
+article = [
+    {
+    "title": "Újratervezés program",
+    "about": "Struktúraváltó képzések",
+    "main": "A képzések részletei",
+    "tags": "SV"
+    },
+
+    {
+    "title": "Új cím",
+    "about": "Struktúraváltó képzések",
+    "main": "A képzők részletei",
+    "tags": "SV3, 2023."
+
+    }
+]
+tags = {
+    "tag": "mitast"
 }
